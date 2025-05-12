536be9784d11114ba12c88d9edf3c6510b3cd8b8
0x46641766F7572 <96478305+favourolaoye@users.noreply.github.com>
2025-05-12 09:56:25 +0100

    init

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const productRoutes = require('./routes/productRoutes');
-
+const cors = require("cors");
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 // Routes
 app.use('/api/products', productRoutes);
